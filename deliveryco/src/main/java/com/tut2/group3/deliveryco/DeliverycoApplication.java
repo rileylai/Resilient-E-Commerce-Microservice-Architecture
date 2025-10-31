156fc4de275604b995d983e95475e200ac3fa737
Riley Lai <rileylai1101@gmail.com>
2025-11-01 00:05:18 +1100

    Fix concurrent order delivery processing
    Enable async processing in DeliveryScheduler to handle multiple orders simultaneously

--- deliveryco/src/main/java/com/tut2/group3/deliveryco/DeliverycoApplication.java
+++ deliveryco/src/main/java/com/tut2/group3/deliveryco/DeliverycoApplication.java
@@ -2,6 +2,7 @@ package com.tut2.group3.deliveryco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
@@ -14,6 +15,7 @@ import java.util.TimeZone;
  */
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class DeliverycoApplication {
 
     public static void main(String[] args) {
